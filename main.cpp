a300b3be5acabdf0c04bcc70735dc4bbdc15723a
tuli273 <tulio.ruiz@udea.edu.co>
2022-04-05 03:26:18 -0500

    Informe Modelamiento Juego

--- main.cpp
+++ main.cpp
@@ -2,10 +2,19 @@
 
 #include <QApplication>
 
+#include<QSplashScreen>
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+    //Con ayuda de la libreria QSplashScreen se puede mostrar una pantalla de carga con un logo png diseñado para el juego
+    QSplashScreen splash;
+    splash.setPixmap(QPixmap(":/recursos/imagenes/presentacion").scaled(308,274));
+    splash.show();
+    //se muestra por 3 segundos el logo del juego y luego se ejecuta el juego
+    QTimer::singleShot(3000,&splash,SLOT(close()));
+    QTimer::singleShot(3500,&w,SLOT(show()));
     return a.exec();
 }
--- mainwindow.h
+++ mainwindow.h
@@ -1,8 +1,22 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
-
-#include <QMainWindow>
-
+#include <QGraphicsScene> //incluir la libreria QgraphicsScene.
+#include "tanque.h" //incluir la clase tanque.
+#include <QMainWindow> //incluir libreria mainWindow
+#include "fuerzaaerea.h" //incluir la clase fuerza Aerea enemiga.
+#include "balas.h" //incluir la libreria balas.
+#include "salud.h" //incluir la libreria de salud.
+#include "puntaje.h" //incluir la libreria de puntaje.
+#include <QKeyEvent> //libreria para las teclas.
+#include <QTimer> //lireria del timer.
+#include <QDebug> //libreria de debug.
+#include <QList> //libreria de Qlist.
+#include <fstream> //libreria de lectura de archivos.
+#include <QFile> //libreria lectura de archivos.
+#include <QTextStream> //libreria flujo de texto
+#include <QMessageBox> //libreria de mensajes de caja
+#include <QMediaPlayer> //libreria para musica.
+#include <QMovie> //libreria para gif.
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
