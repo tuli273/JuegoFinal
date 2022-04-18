7aedaf0323ea032bdda839c018087c7fb44af690
tuli273 <tulio.ruiz@udea.edu.co>
2022-04-18 15:56:19 -0500

    Se configuran los .h y el ui

--- fuerzaaerea.h
+++ fuerzaaerea.h
@@ -1,11 +1,77 @@
 #ifndef FUERZAAEREA_H
 #define FUERZAAEREA_H
+#include <QGraphicsItem> //libreria de item grafico
+#include <QPainter> //libreria para pintar
+#include <math.h> //libreria matematica
+#include <QPixmap> //libreria Qpixmap
+#include <QGraphicsPixmapItem> //libreia QgraphicsPixmapItem
+#include <QDebug> //libreria de debug.
+
+using namespace std;
 
 
-class fuerzaAerea
+class fuerzaaerea:public QGraphicsItem //heredar las funciones de QgraphicsItem.
 {
-public:
-    fuerzaAerea();
-};
+    double posx; //posicion en x del enemigo.
+    double posy; //posicion en y del enemigo.
+    double ancho; //ancho del enemigo.
+    double largo; //largo del enemigo.
+    double columnas=0; //columnas
+    double filas=0; //filas
+    double vel; //velocidad del fuerzaaerea
+    double velx; //velocidad en el eje x
+    double vely;  //velocidad en el eje y
+    double g=9.8; //gravedad
+    double delta=0.1; //timepo
+    double pi=3.1416; //pi
+    double angulo; //angulo
+    int dir; //direccion 1 es hacia la derecha, direccion 2 es a la izquierda
+    int r; //radio para el MCU
+    double Rad= 0.01745329252; //radianes
+    double i=0; // variable para variar los radianes
+    double W; //velocidad angular
+    double velocidad_rotacion=0; //velocidad angular
+    QPixmap *pixmap; //variable pixmap
+    int imagen; //variable para seleccionar imagen
 
+public:
+    fuerzaaerea(); //constructor por defecto
+    fuerzaaerea(double x, double y, double ancho_, double largo_, double vi, double ang_); //sobre carga de constructor
+    fuerzaaerea(double x, double y, double ancho_, double largo_); //sobre carga deconstructor
+
+    double getPosx(); //funcion para tomar la posicion en x.
+    double getPosy(); //funcion para tomar la posicion en y.
+
+    double getVelx(); //funcion para tomar la velocidad en x.
+    double getVely(); //funcion para tomar la velocidad en y.
+    void setVelx(double vx_); //funcion para dar la velocdad en x
+    void setVely(double vy_); //funcion para dar la velocdad en y
+
+    void actualizarposicion_derecha(); //funcion que actualiza la posicion hacia la derecha
+    void actualizarposicion_izquierda(); //funcion que actualiza la posicion hacia la izquierda
+    void rebotepiso(); //funcion que simula rebote con el piso
+
+    void setVel(double vel_); //funcion que da velocidad al fuerzaaerea
+    double getVel(); //funcion obtener la velocidad.
+
+    double getAngulo(); //funcion obtener el angulo.
+    void setAngulo(double Angulo); //funcion para cambiar el angulo
+
+    void setImagen(int imagen_); //funcion para seleccionar la imagen
+
+    void actualizarvelocidad(); //funcion que actualizia la velocidad
+
+    void MCU(double x, double y, int r_, double W_, double Desfase_); //movimentiento circular uniforme
+    void MCU_horizontal(double x, double y, int r_, double W_, double Desfase_);
+    void MAS(double x, double y, int r_, double W_, int d_); //movimiento armonico simple
+
+    void rotar(int r_); //funcion que rota el sprite del fuerzaaerea
+
+    void setColumnas_fila(int c_, int f_); //funcion para seleccionar las columnas y las filas
+
+    void setDir(int dir_); //funcion para cambiar la direccion.
+    int getDir(); //funcion para tomar la direccion.
+    QRectF boundingRect() const; //funcion que dibuja
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget); //funcion que pinta
+};
 #endif // FUERZAAEREA_H
--- puntaje.h
+++ puntaje.h
@@ -1,11 +1,19 @@
 #ifndef PUNTAJE_H
 #define PUNTAJE_H
+#include <QGraphicsTextItem> //libreria de QgraphicsTextItem
+#include <QFont> //libreria de fuentes
 
-
-class puntaje
+class puntaje: public QGraphicsTextItem
 {
+    int Puntaje=0; //variable que guarda el puntaje
 public:
-    puntaje();
+    puntaje(QGraphicsItem * parent=0); //constructor
+    void aumentar_puntaje(int p_); //funcion para aumentar el puntaje
+    void setPuntaje(int p_); //funcion para setear el putaje
+    int getPuntaje(); //funcion para obetener el puntaje
+    void mostrar_Puntaje(); //funcion para mostrar el puntaje
+    void tiempo(int p_); //funcion para aumentar el puntaje
+    void texto(QString texto); //funcion mostrar algun texto en pantalla
 };
 
 #endif // PUNTAJE_H
