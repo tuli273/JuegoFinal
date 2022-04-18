b0bd169768b1526528c94baee4d4838c265d39d5
tuli273 <tulio.ruiz@udea.edu.co>
2022-04-18 15:33:53 -0500

    se crean las balas

--- balas.h
+++ balas.h
@@ -1,11 +1,20 @@
 #ifndef BALAS_H
 #define BALAS_H
+#include <QGraphicsPixmapItem> //incluir la libreria QgraphicsPixmapItem.
+#include "fuerzaaerea.h" //incluir la libreria enemigo
+#include "puntaje.h" //incluir la libreria puntaje
 
+//clase balas
 
-class balas
+class balas: public QObject, public QGraphicsPixmapItem //heredar las librerias
 {
+    Q_OBJECT //agregar un Q_OBJECT
 public:
-    balas();
+    balas(QGraphicsItem *parent = 0); //crear las balas
+
+public slots:
+    void move(); //funcion mover las balas
 };
 
 #endif // BALAS_H
+
